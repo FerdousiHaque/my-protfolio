4ac2c8b4970e089398bb9233aa316ccfeb1cf57f
FerdousiHaque <ferdousi160228@gmail.com>
2020-09-23 19:14:59 +0600

    Updates

---
+++ precache-manifest.7a07766d8726356ca2a2989f8d4aa56f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083d336f2a3288ad19d83598da302141",
+    "url": "/FerdousiHaque/my-protfolio/index.html"
+  },
+  {
+    "revision": "1a31567de5f285a4aae4",
+    "url": "/FerdousiHaque/my-protfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "839f84162e6008e3d40c",
+    "url": "/FerdousiHaque/my-protfolio/static/css/main.c3cbed3f.chunk.css"
+  },
+  {
+    "revision": "1a31567de5f285a4aae4",
+    "url": "/FerdousiHaque/my-protfolio/static/js/2.53cf120d.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/FerdousiHaque/my-protfolio/static/js/2.53cf120d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839f84162e6008e3d40c",
+    "url": "/FerdousiHaque/my-protfolio/static/js/main.3f7ac9e1.chunk.js"
+  },
+  {
+    "revision": "e6bbd1b888dd0e581706",
+    "url": "/FerdousiHaque/my-protfolio/static/js/runtime-main.70708419.js"
+  }
+]);
