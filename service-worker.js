5cf324848509ca0b30c3f5c1ee9d0c5f5ef60fa0
FerdousiHaque <ferdousi160228@gmail.com>
2020-09-23 19:58:54 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FerdousiHaque/my-protfolio/precache-manifest.7a07766d8726356ca2a2989f8d4aa56f.js"
+  "/my-protfolio/precache-manifest.a65e6e8baddcb87fea2f9eec452b55a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FerdousiHaque/my-protfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-protfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
